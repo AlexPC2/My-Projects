20e74255e49a08ff18bc8c31923694e7e0bedbb3
AlexNoyanov <alex.noyanov@gmail.com>
2017-11-27 23:59:46 +0300

    For and While cycles lesson

---
+++ С lessons/While_for_cycles/While_for_cycles/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  While_for_cycles
+//
+//  Created by Александр Ноянов on 27.11.17.
+//  Copyright © 2017 Popoff Developer Studio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

985bdcc159a1f60e89e938a33ccb1ccda952ef1a
AlexPC#2 <alex.noyanov2@gmail.com>
2017-12-02 17:31:31 +0300

    New comments added and "print".

--- Create_files/Create_files/Create_files.cpp
+++ Create_files/Create_files/Create_files.cpp
@@ -4,10 +4,10 @@
 // Create_files.cpp: определяет точку входа для консольного приложения.
 //
 
-#include "stdafx.h"
-#include <iostream>
-#include <fstream>
-#include <ctime>
+#include "stdafx.h"		// Стандартная библиотека
+#include <iostream>		// Библиотека стандартных функций
+#include <fstream>		// Библиотека для  работы с файлами
+#include <ctime>		// Библиотека времени
 using namespace std;
 
 int main(int argc,char* argv[])
@@ -15,6 +15,9 @@ int main(int argc,char* argv[])
 	setlocale(LC_ALL,"rus");
 	char buffer[128];	// Буффер для данных из файла
 	time_t t = time(0);
+
+	printf("Program working..."); // Печатаем, что программа работает
+
 /*
 	struct tm * now = localtime(&t);
 	cout << (now->tm_year +1900) << '-'
@@ -23,33 +26,31 @@ int main(int argc,char* argv[])
 		<< endl;
 		*/
 
-	ofstream fout("D:\Files\Result.txt");
+	ofstream fout("D:\Files\Result.txt");   // Путь к файлу
 	fout << "Этот текст был записан в этот файл из программы. Проект Create_files.cpp в Visual Studio \n Тест перевода строки в файле \Перевод строк работает нормально\n";
 	fout << "Вывод чисел\n";
 
 	for (int i = 0; i < 10;i++) {
-		fout << i;
+		fout << i;				// << Означает вывод 
 	}
 
 	fout << "Вывод чисел работает";
 
-	fout.close();
+	fout.close();				// Закрываем файл
 
 	setlocale(LC_ALL, "rus");	// корректное отображение Кириллицы
 	char buff[128];				// буфер промежуточного хранения считываемого из файла текста
 	ifstream fin("MyFile.txt"); // открыли файл для чтения
 
 	fin >> buff;				// считали первое слово из файла
-	cout << buff << endl;		 // напечатали это слово
+	cout << buff << endl;	    // напечатали это слово
 
 	fin.getline(buff, 128); // считали строку из файла
 	fin.close();			// закрываем файл
 	cout << buff << endl;   // напечатали эту строку
 
-	//system("pause");
 	system("pause");
     return 0;
 }
 // Comment from browser!
 // Comment from AlexPC#2!
-

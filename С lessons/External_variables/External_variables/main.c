3cb03dc7232cf62ffe3de67b4289e4ac206a496e
Alex Noyanov <alex.noyanov@gmail.com>
2017-12-05 14:48:42 +0300

    Reverse Poland logical calculator. Lesson about external variables. Paragraph 3.4

---
+++ С lessons/External_variables/External_variables/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  External_variables
+//
+//  Created by Alex Noyanov on 05.12.17.
+//  Copyright © 2017 Popoff Developer Studio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

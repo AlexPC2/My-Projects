2603ee43e09760338cd33837b5300aff8e7d9d80
Alex Noyanov <alex.noyanov@gmail.com>
2017-12-05 14:49:37 +0300

    Program about Poland reverse logic calculation.

--- С lessons/External_variables/External_variables/main.c
+++ С lessons/External_variables/External_variables/main.c
@@ -6,10 +6,74 @@
 //  Copyright © 2017 Popoff Developer Studio. All rights reserved.
 //
 
+//
+
 #include <stdio.h>
+#include <stdlib.h> // For atof()
+
+/*
+#define (can be as mch as we want)
+ 
+ void push(double f) {...}
+ 
+ double pop(void) {...}
+ 
+ int getpop(char s[]) {...}
+ 
+*/
+
+#define MAXOP 100   // Maximum operand size
+#define NUMBER '0'  // Number
+
+
+int getop(char[]);
+void push(double);
+double pop(void);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+
+    int type;
+    double op2;
+    char s[MAXOP];
+    
+    while ((type = getop(s)) != EOF) {
+        
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+                
+            case '+':
+                push(pop() + pop());
+                break;
+            
+            case '*':
+                push(pop() * pop());
+                break;
+                
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            
+            case '/':
+                op2 = pop();
+                if(op2 != 0.0)
+                  push(pop() / op2);
+                else
+                    printf("Error: Can't on zero");
+                break;
+                
+            case '\n':
+                printf("\t%.8g\n",pop());
+                break;
+                
+            default:
+                printf("Unknown operation!");
+                break;
+        }
+    }
+    
     return 0;
 }

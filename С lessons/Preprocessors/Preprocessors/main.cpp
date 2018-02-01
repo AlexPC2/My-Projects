962ddb180e34cc41062ecfdf951c835da328f07b
AlexNoyanov <alex.noyanov@gmail.com>
2018-02-01 14:17:47 +0300

    Paragraph 4.11.3 about Conditional compilation and all C preprocess operarors

--- С lessons/Preprocessors/Preprocessors/main.cpp
+++ С lessons/Preprocessors/Preprocessors/main.cpp
@@ -9,6 +9,9 @@
 //  Commands with # called preprocessors operators
 
 #include <iostream>
+#include <ctype.h>
+
+
 
 // #include<fileName> or #include"fileName"
 
@@ -20,13 +23,62 @@
 // #define first second
 // First emplemantion will be changed by second and can be used everywhere
 
-
 #define max(A,B) ((A) > (B) ? (A):(B))
 
 // Then x = max(p+q,r+s) will be replaced:
 // x = ((p+q) > (r+s) ? (p+q):(r+s))
+
+//  BUT:
+//  max(i++,j++) is WRONG!
+
+// For UNDEFINE some defined expression can be used #undef
+
+#undef getchar
+// int getchar (void){
+// }
+
+
+#undef dprint(expr) printf(#expr "= %g\n",expr)
+
+
+#define paste(front,back) front ## back
+
+// Exercise:
+
+// This function can actually swap two values x and y with type t
+#define swap(t,a,b) (t valueX = x; x = y; y = valueX)
+
+//  === Сonditional compilation  ===
+#if  !defined(HDR)
+#define HDR
+/*
+ here can be resources from file hdr.h
+ */
+#endif
+
+
+// This following instructons can detect what files not included here
+#if SYSTEM == SYSV
+    #define HDR "sysv.h"
+#elif SYSTEM == BSD
+    #define HDR "bsd.h"
+#elif SYSTEM == MSDOS
+    #define HDR "msdos.h"
+#else
+    #define HDR "default.h"
+#endif
+#include HDR
+
+
+// Instructions #ifdef and #ifndef can check does some expressions defined/undefined
+#ifndef HDR
+#define HDR
+/* here is file resource could be */
+#endif
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+  
+    
+    
 }

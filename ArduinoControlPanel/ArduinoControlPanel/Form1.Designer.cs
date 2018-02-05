ab2de8b555c9efe96cda008968ccaf242a32a329
AlexPC#2 <alex.noyanov2@gmail.com>
2018-02-05 23:47:54 +0300

    Button for connection was deletd. Now trying to get all available serial ports

--- ArduinoControlPanel/ArduinoControlPanel/Form1.Designer.cs
+++ ArduinoControlPanel/ArduinoControlPanel/Form1.Designer.cs
@@ -47,7 +47,6 @@
             this.Main1Button = new System.Windows.Forms.Button();
             this.Main2Button = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -220,24 +219,12 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Action2);
             // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(12, 165);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(109, 41);
-            this.button2.TabIndex = 17;
-            this.button2.Text = "Connect";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.GetConnected);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.ClientSize = new System.Drawing.Size(875, 528);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Main2Button);
             this.Controls.Add(this.Main1Button);
@@ -282,7 +269,6 @@
         private System.Windows.Forms.Button Main1Button;
         private System.Windows.Forms.Button Main2Button;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
--- ArduinoControlPanel/ArduinoControlPanel/Form1.cs
+++ ArduinoControlPanel/ArduinoControlPanel/Form1.cs
@@ -39,6 +39,79 @@ namespace ArduinoControlPanel
             TurnTheLedOFF.Enabled = false;
         }
 
+        //  === CHOOSING SERIAL PORT===
+        /*
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            string[] myPort;
+
+            int COM1 = cbCommPorts.SelectedIndex;
+            int COM2 = cbCommPorts.SelectedIndex;
+            int COM3 = cbCommPorts.SelectedIndex;
+            Object selectedItem = serialPort1.PortName;
+
+            myPort = System.IO.Ports.SerialPort.GetPortNames();
+            cbCommPorts.Items.AddRange(myPort);
+
+            serialPort1.PortName = cbCommPorts.SelectedItem.ToString();
+            serialPort1.BaudRate = 115200;
+
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.PortName = cbCommPorts.SelectedItem.ToString();
+
+                serialPort1.Open();
+                btnTransmit.Enabled = true;
+                btn2.Enabled = true;
+                btn3.Enabled = true;
+            }
+
+            ====     ====
+    */
+
+        // public partial class Form1 : Form
+        // {
+
+            /*
+        public Form1()
+            {
+                InitializeComponent();
+                this.Load += Form1_Load;
+            }
+
+            void Form1_Load(object sender, EventArgs e)
+            {
+                var ports = SerialPort.GetPortNames();
+                cmbSerialPorts.DataSource = ports;
+            }
+
+            private void btnOk_Click(object sender, EventArgs e)
+            {
+                if (cmbSerialPorts.SelectedIndex > -1)
+                {
+                    MessageBox.Show(String.Format("You selected port '{0}'", cmbSerialPorts.SelectedItem));
+                    Connect(cmbSerialPorts.SelectedItem.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("Please select a port first");
+                }
+            }
+
+            private void Connect(string portName)
+            {
+                var port = new SerialPort(portName);
+                if (!port.IsOpen)
+                {
+                    port.BaudRate = 19200;
+                    port.Open();
+                    //Continue here....
+                }
+            }
+       // }
+       */
+        //  ====
+
         public Form1()
         {
             InitializeComponent();
@@ -101,9 +174,9 @@ namespace ArduinoControlPanel
 
         }
 
-        private void GetConnected(object sender, EventArgs e)   // Button for connecting
-        {
-            myport.WriteLine("Connect!");       //Send command to controller
-        }
+       // private void GetConnected(object sender, EventArgs e)   // Button for connecting
+       // {
+       //     myport.WriteLine("Connect!");                       //Send command to controller
+       // }
     }
 }

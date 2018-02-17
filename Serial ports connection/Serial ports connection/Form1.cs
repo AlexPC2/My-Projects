f49c08a6e97f1c5eee27295f2b63c28995e56a72
AlexPC#2 <alex.noyanov2@gmail.com>
2018-02-17 16:23:32 +0300

    С++ example counting sum of numerals in  numbers

--- Serial ports connection/Serial ports connection/Form1.cs
+++ Serial ports connection/Serial ports connection/Form1.cs
@@ -12,10 +12,48 @@ namespace Serial_ports_connection
 {
     public partial class Form1 : Form
     {
+
+        
         public Form1()
         {
             InitializeComponent();
+            this.Load += Form1_Load;
+
         }
+
+        //  Bugs don't fixed here:
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            var ports = SerialPort.GetPortNames();
+            cmbSerialPorts.DataSource = ports;
+        }
+
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (cmbSerialPorts.SelectedIndex > -1)
+            {
+                MessageBox.Show(String.Format("You selected port '{0}'", cmbSerialPorts.SelectedItem));
+                Connect(cmbSerialPorts.SelectedItem.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Please select a port first");
+            }
+        }
+
+        private void Connect(string portName)
+        {
+            var port = new SerialPort(portName);
+            if (!port.IsOpen)
+            {
+                port.BaudRate = 19200;
+                port.Open();
+                //Continue here....
+            }
+        }
+
 
         private void button1_Click(object sender, EventArgs e)  // When button "connect" pressed:
         {

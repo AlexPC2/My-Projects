71fee5468bf102c453c659e38628dcde3470e776
AlexPC#2 <alex.noyanov2@gmail.com>
2018-02-17 17:07:10 +0300

    Input your numbers countless times

--- Sum-Example1/Sum-Example1/Sum-Example1.cpp
+++ Sum-Example1/Sum-Example1/Sum-Example1.cpp
@@ -9,29 +9,39 @@ using namespace std;
 int main()
 {
 
-	int b, c, n;
-
-	int flag = 0;
-
-	int sum = 0, sum1 = 0, sum2 = 0;
-	cout << "Input your 3 numbers\n";		//Print message to input
 	
-	cin >> n;								//Read n
 
-	while (n > 0)
-	{
-		if ((n / 10) > 0)
-			cout << n % 10 << "+";
-		else
-			cout << n % 10;
-		sum += n % 10;
-		n /= 10;
+		int b, c, n;
+
+		int sum = 0, sum1 = 0, sum2 = 0;
+		cout << "Input your 3 numbers\n";		//Print message to input
+
+
+		n = 2;
+
+		while (n != 1)
+		{
+			sum = 0;
+			sum1 = 0;
+			sum2 = 0;
+
+		cin >> n;								//Read n
+
+		while (n > 0)
+		{
+			if ((n / 10) > 0)
+				cout << n % 10 << "+";
+			else
+				cout << n % 10;
+			sum += n % 10;
+			n /= 10;
+		}
+		cout << "=" << sum;
+		//cout << "\n";
+		cin >> n;
+		cout << "Input your 3 numbers\n";		//Print message to input
 	}
 
-	cout << "=" << sum;
-
-	cout << "\n";
-	cin >> n;
     return 0;
 }
 

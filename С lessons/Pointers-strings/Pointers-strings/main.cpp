6a90c223c36d11d3e713c6fe2157543f90994cfd
AlexNoyanov <alex.noyanov@gmail.com>
2018-02-18 18:28:07 +0300

    New projects about Pointers in C language

---
+++ С lessons/Pointers-strings/Pointers-strings/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pointers-strings
+//
+//  Created by Александр Ноянов on 18.02.18.
+//  Copyright © 2018 ANLab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

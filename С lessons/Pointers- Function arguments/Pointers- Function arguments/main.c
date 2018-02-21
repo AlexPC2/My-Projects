75101b727f28ab8249402c8dd839f1ff13451d1a
Alex Noyanov <alex.noyanov@gmail.com>
2018-02-21 11:34:42 +0300

    Examples about pointers and function arguments

---
+++ С lessons/Pointers- Function arguments/Pointers- Function arguments/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Pointers- Function arguments
+//
+//  Created by Alex Noyanov on 09.02.18.
+//  Copyright © 2018 Popoff Developer Studio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

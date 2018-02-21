ffc632f9495f92bf97b918f00a5a5ff0098a4b09
Alex Noyanov <alex.noyanov@gmail.com>
2018-02-21 11:37:33 +0300

    New C lesson programs

--- С lessons/Pointers- Function arguments/Pointers- Function arguments/main.c
+++ С lessons/Pointers- Function arguments/Pointers- Function arguments/main.c
@@ -7,6 +7,19 @@
 //
 
 #include <stdio.h>
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+/* getint: читает следующее целое из вводы в *pn */
+int getint( int *pn){
+    
+    int c,sign;
+    
+    while(isspace(c = getch()))
+        ; /* Пропуск символов разделителей */
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...

4e61750fb3d149acad9f1e8256eeeabc7a1979d0
AlexPC#2 <alex.noyanov2@gmail.com>
2018-03-01 19:35:12 +0300

    Added function working with two-dementional arrays.

--- Multidementional arrays/Multidementional arrays/Multidementional arrays.cpp
+++ Multidementional arrays/Multidementional arrays/Multidementional arrays.cpp
@@ -12,6 +12,12 @@ static char daytab[2][13] = {
 	{ 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31 }
 };
 
+//	Two dementional array example:
+/*	array[i][k];				*/
+//	But:
+/*	array[i,k]	-	IS WRONG!	*/
+
+
 /* days_of_year: определ€ет день года по мес€цу и дню */
 int day_of_year(int year, int month, int day)
 {
@@ -23,6 +29,18 @@ int day_of_year(int year, int month, int day)
 	return day;
 }
 
+
+/*	month_day: определ€ет мес€ц и днеь по дню года	*/
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+	int i, leap;
+
+	leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+	for (i = 0; yearday > daytab[leap][i]; i++)
+		yearday -= daytab[leap][i];
+	*pmonth = i;
+	*pday = yearday;
+}
 
 
 

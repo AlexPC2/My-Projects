3ec4490528a97036af5d88495d11cb8d0fa8e857
AlexPC#2 <alex.noyanov2@gmail.com>
2018-03-01 20:52:06 +0300

    Arduino control panel project

---
+++ С lessons/Arrays of pointers/Arrays of pointers/Arrays of pointers.cpp
@@ -0,0 +1,21 @@
+// Arrays of pointers.cpp: определ€ет точку входа дл€ консольного приложени€.
+//
+
+//	Chapter5.8-9
+
+
+#include "stdafx.h"
+
+/*	month_name: возвращает им€ n-го мес€ца*/
+char *month_name(int n)
+{
+	static char *name[] = {
+		"Ќеверный мес€ц"
+	}
+}
+
+int main()
+{
+    return 0;
+}
+

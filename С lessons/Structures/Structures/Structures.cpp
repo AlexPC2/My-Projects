f55ceabb3bf8eea1f4b86aef9617808edfd64fc4
AlexPC#2 <alex.noyanov2@gmail.com>
2018-03-08 01:57:10 +0300

    addpoint function added

--- С lessons/Structures/Structures/Structures.cpp
+++ С lessons/Structures/Structures/Structures.cpp
@@ -34,11 +34,13 @@ screen.pt2 = makepoint( XMAX, YMAX);
 middle = makepoint((screen.pt1.x + screen.pt2.x)/2,
 					(screen.pt1.y+screen.pt2.y)/2);
 
-
 /*	addpoint: сложение двух точек*/
-struct point addpoint(struct point p1, struct point p2);
-
-
+struct point addpoint(struct point p1, struct point p2)
+{
+	p1.x += p2.x;
+	p1.y += p2.y;
+	return p1;
+}
 
 int main()
 {

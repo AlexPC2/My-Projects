c8e3ca57279586d1734c9c096199185493d2cb01
AlexNoyanov <alex.noyanov@gmail.com>
2018-07-23 17:52:42 +0300

    Lesson about maps in cpp language

--- CPP lessons/Map/Map/main.cpp
+++ CPP lessons/Map/Map/main.cpp
@@ -14,11 +14,35 @@
 #include <map>
 using namespace std;
 
+// Printing this map
 void PrintMap(const map<int,string>& m){
     for(auto s:m)
     cout << s.first << ": " << s.second << endl;
     
 }
+
+// Print map size:
+void PrintMapSize(const map<int,string>& m){
+    cout << "Size = " << m.size() << endl;
+}
+
+
+// Creating reverse map:
+map<string,int> BuildReverseMap(const map<int,string>& m){
+    map<string,int> result; // Result map
+    for(auto item:m){
+        result[item.second] = item.first;   // Set the following map the number from the string
+    }
+    return result;
+}
+
+// To print reverse map:
+void PrintReverseMap(const map<string,int>& m){
+    for(auto item:m){
+        cout << item.first << " : " <<item.second;
+    }
+}
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -30,6 +54,16 @@ int main(int argc, const char * argv[]) {
     
     // Printing map
     PrintMap(events);
+    PrintMapSize(events);
+    
+    // Find the following string from the key:
+    cout << events[1950] << endl;
+    
+    // Delete value:
+    events.erase(1950); // Deleting value from this key
+    
+    // Print reverse map:
+    PrintReverseMap(BuildReverseMap(events));
     
     return 0;
 }

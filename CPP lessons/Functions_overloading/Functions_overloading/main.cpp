791ceba5d0942b7c071ce017613a05d15349a428
AlexNoyanov <alex.noyanov@gmail.com>
2018-08-25 00:11:23 +0300

    Functions overloading

--- CPP lessons/Functions_overloading/Functions_overloading/main.cpp
+++ CPP lessons/Functions_overloading/Functions_overloading/main.cpp
@@ -7,6 +7,10 @@
 //
 
 #include <iostream>
+#include <math.h>
+#include <stdio.h>
+
+using namespace std;
 
 // Перегрузка функций - создание нескольких функций, возвращающих или принимающих разные типы, но имеющие одинаковое имя
 // Перезагрузка позволяет создавать универсальные одноименные функции, принимающие или возвращающие разные типы данных
@@ -28,16 +32,60 @@ void f2(char s);
 //                                   === Упражнения ===
 // 1) Создать перегруженную функцию sroot(), которая возвращает квадратный корень своего аргумента:
 int sroot(int x){
-    return x*x;
+    return sqrt(x);
 }
 
 double sroot(double x){
-    return x*x;
+    return sqrt(x);
 }
 
 float sroot(float x){
-    return x*x;
+    return sqrt(x);
 }
+
+
+// 3) Создать функцию min, возвращающую меньшее из принимаемых значений
+int min(int a, int b){
+    if(a < b)
+        return a;
+    else
+        return b;
+}
+
+float min(float a, float b){
+    if(a < b)
+        return a;
+    else
+        return b;
+}
+double min(double a, double b){
+    if(a < b)
+        return a;
+    else
+        return b;
+}
+
+char min(char a,char b){
+    if(a < b)
+        return a;
+    else
+        return b;
+}
+
+
+
+// 4) Функция задержки sleep
+void sleep(int seconds){
+    
+}
+/*
+void Sleep(BYTE time)
+{
+    return Sleep((DWORD)time*1000);
+}
+void sleep(string seconds){
+    
+}*/
 
 int main(int argc, const char * argv[]) {
     // insert code here...

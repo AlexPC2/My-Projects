571f3c35f6e7d0e4dbd399890c2ec78f9d25aa77
Alex Noyanov <alex.noyanov@gmail.com>
2018-08-29 01:16:09 +0300

    Classes usage example

--- С lessons/Types_transfromation/Cycle_deck/Cycle_deck/main.cpp
+++ С lessons/Types_transfromation/Cycle_deck/Cycle_deck/main.cpp
@@ -8,8 +8,67 @@
 
 #include <iostream>
 
+using namespace std;
+
+#define SIZE 10
+
+class deck{
+    
+    int dck[SIZE];
+    int tos;
+    
+public:
+    void init();              // Deck initializing
+    void push(int value);     // Push new int in to the deck
+    int pop();               // Pop value from the deck
+};
+
+void deck::init(){
+    tos = 0;
+}
+
+void deck::push(int value){
+    if(tos == SIZE)
+        tos = 0;
+    dck[tos] = value;
+    tos++;
+}
+
+int deck::pop(){
+    if(tos == 0){
+        cout << "=== Deck is empty! ===";
+        return 0;
+    }
+    tos--;
+    return dck[tos];
+    
+}
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    //char s[80];
+    
+    //cout << "Length is:";
+    //cin >> s;
+    
+    deck myDeck;
+    myDeck.init();
+    for(int i = 0;i<10;i++)
+        myDeck.push(i);
+    
+    cout << "Deck fully loaded: ";
+    
+    for(int i = 0;i<SIZE;i++)
+        cout << myDeck.pop();
+    
+    for(int i = 0;i<20;i++)
+        myDeck.push(i);
+    
+    cout << "Deck overloaded: ";
+    
+    for(int i = 0;i<SIZE;i++)
+        cout << myDeck.pop();
+    
     return 0;
 }

19213a7e1267b980ae5b632b8a19b131fcb25810
AlexPC#2 <alex.noyanov2@gmail.com>
2018-10-31 23:50:51 +0300

    C lesson about structures

--- С lessons/Structures/Structures/Structures.cpp
+++ С lessons/Structures/Structures/Structures.cpp
@@ -11,11 +11,11 @@ struct point {
 };
 
 struct rect {
-	struct point pt1;
-	struct point pt2;
+	point pt1;
+	point pt2;
 };
 
-struct point makepoint(int x, int y)
+point makepoint(int x, int y)
 {
 	struct point temp;
 
@@ -24,15 +24,10 @@ struct point makepoint(int x, int y)
 	return temp;
 };
 
-struct rect screen;
+rect screen;
 struct point middle;
 struct point makepoint(int,int);
 
-screen.pt1 = makepoint( 0,0 );
-screen.pt2 = makepoint( XMAX, YMAX);
-
-middle = makepoint((screen.pt1.x + screen.pt2.x)/2,
-					(screen.pt1.y+screen.pt2.y)/2);
 
 /*	addpoint: сложение двух точек*/
 struct point addpoint(struct point p1, struct point p2)
@@ -40,10 +35,25 @@ struct point addpoint(struct point p1, struct point p2)
 	p1.x += p2.x;
 	p1.y += p2.y;
 	return p1;
+
+	//	Эта функция принимает и возвращает структуру point
 }
+
+/*	prinrect: возвращает 1, если p в r, и 0 в противном случае	*/
+int prinrect(struct point p, struct rect r)
+{
+	
+}
+
 
 int main()
 {
-    return 0;
+	screen.pt1 = makepoint(0, 0);
+	screen.pt2 = makepoint(XMAX, YMAX);
+
+	middle = makepoint((screen.pt1.x + screen.pt2.x) / 2,
+		(screen.pt1.y + screen.pt2.y) / 2);
+	
+	return 0;
 }
 

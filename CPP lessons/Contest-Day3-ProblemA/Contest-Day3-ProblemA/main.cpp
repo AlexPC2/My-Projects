8f52cc230f1eda57c585c37a45fc9162df1bffd6
Alex Noyanov <alex.noyanov@gmail.com>
2019-03-14 00:11:57 +0300

    C++ lesson from ICPC contests. Problem A division B

---
+++ CPP lessons/Contest-Day3-ProblemA/Contest-Day3-ProblemA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Contest-Day3-ProblemA
+//
+//  Created by Alex Noyanov on 12.03.19.
+//  Copyright © 2019 Popoff Developer Studio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

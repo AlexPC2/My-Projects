2a7e4ed97972f6670ce0e8a6b0cb0a8e411df28e
Alex Noyanov <alex.noyanov@gmail.com>
2019-05-08 22:50:28 +0300

    Some C++ lessons about vectors

---
+++ CPP lessons/Codeforces_811_B/Codeforces_811_B/main.cpp
@@ -0,0 +1,62 @@
+//
+//  main.cpp
+//  Codeforces_811_B
+//
+//  Created by Alex Noyanov on 07.05.19.
+//  Copyright © 2019 Popoff Developer Studio. All rights reserved.
+//
+
+// Codeforces 811 Problem B
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+
+    int n,m;
+    
+    cin >> n >> m;
+    vector<int>a(n);
+    
+    
+    return 0;
+}
+
+/*
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    //std::cout << "Hello, World!\n";
+    int n,m;
+    
+    cin >> n >> m;
+    vector<int>a(n);
+    for(int i = 0; i <n;i++)
+    {
+        cin >> a[i];
+    }
+    
+    for(int i = 0; i<m;i++)
+    {
+        int l, r, x;
+        cin >> l >> r >> x;
+        l--; r--; x--;
+        int old = a[x];
+        
+        int count = 0;
+        for(int j = 1; j <= r; j++){
+            //if()
+        }
+        
+        //vector<int> v = vector<int>(a);
+        //sort(v.begin()+1,v.begin+r+1);
+        
+    }
+    
+    
+    
+    return 0;
+}
+ 
+ */

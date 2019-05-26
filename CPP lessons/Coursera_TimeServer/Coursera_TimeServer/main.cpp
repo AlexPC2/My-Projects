9e4d5f0270b7ecb7f760ff50ac7185f3d7636ee5
AlexNoyanov <alex.noyanov@gmail.com>
2019-05-26 23:07:23 +0300

    The lesson about catching exceptions in the class TimeServer

---
+++ CPP lessons/Coursera_TimeServer/Coursera_TimeServer/main.cpp
@@ -0,0 +1,50 @@
+//
+//  main.cpp
+//  Coursera_TimeServer
+//
+//  Created by Александр Ноянов on 26/05/2019.
+//  Copyright © 2019 MPEI. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+string AskTimeServer() {
+    return "03:25:56";
+   //  throw system_error(error_code());
+  //  throw exception();
+}
+
+class TimeServer {
+public:
+    string GetCurrentTime() {
+        try{
+            string str = AskTimeServer();
+            LastFetchedTime = str;
+            return str;
+        }
+        catch(system_error& e){
+            cout << "Exception" << endl;
+            return LastFetchedTime;
+        }
+    }
+    
+private:
+    string LastFetchedTime = "00:00:00";
+};
+
+int main() {
+    
+    TimeServer ts;
+    try {
+        cout << ts.GetCurrentTime() << endl;
+    } catch (exception& e) {
+        cout << "Exception got: " << e.what() << endl;
+    }
+    return 0;
+}

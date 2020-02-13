0872a25a98316f25078c88bacfb9a405bb774cb0
Alex Noyanov <alex.noyanov@gmail.com>
2020-02-13 12:58:04 +0300

    C++  lesson about lists and Hanoi task with recursion alghorithm

--- CPP lessons/ReverseList/ReverseList/main.cpp
+++ CPP lessons/ReverseList/ReverseList/main.cpp
@@ -7,30 +7,33 @@
 //
 
 #include <iostream>
-
 using namespace std;
 
 struct List{
     int value;
     List* next = NULL;
-    void addElement(int x);
+    void addElement(int x){
+        List newElem;
+        newElem.value = x;
+        next = &newElem;
+        newElem.next = NULL;
+    }
 };
 
-void List::addElement(int x){
-    
-}
-
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    //std::cout << "Hello, World!\n";
-    List myList;
+    // Filling the List with numbers:
+    List myListRoot;
     int n;
     cout << "=== Input numbers of list elements: ===" << "\n";
     cin >> n;
-    
+    int v;
     for(int i = 0; i<n;i++){
-        
+        cout << "Input element:\n";
+        cin >> v;
+        myListRoot.addElement(v);
     }
+    
+    // Printing from the 
     
     
     return 0;
